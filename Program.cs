8d712a0ffaaba699a950445f86f62c21ddcc1123
海鹏 郭 <386348907@qq.com>
2020-03-31 09:39:43 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQClient1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Start");
+            IConnectionFactory connFactory = new ConnectionFactory//创建连接工厂对象
+            {
+                HostName = "172.16.77.177",//IP地址
+                Port = 5672,//端口号
+                UserName = "guohp",//用户账号
+                Password = "546526"//用户密码
+            };
+            using (IConnection conn = connFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    String queueName = String.Empty;
+                    if (args.Length > 0)
+                        queueName = args[0];
+                    else
+                        queueName = "GetMark";//消息名称   接收端名称与之相同才会接收
+                    //声明一个队列
+                    channel.QueueDeclare(
+                      queue: queueName,//消息队列名称
+                      durable: false,//是否缓存
+                      exclusive: false,
+                      autoDelete: false,
+                      arguments: null
+                       );
+                    //创建消费者对象
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+
+                        Console.WriteLine(ea.RoutingKey);
+                        byte[] message = ea.Body;//接收到的消息
+                        Console.WriteLine("接收到信息为:" + Encoding.UTF8.GetString(message));
+                    };
+                    //消费者开启监听
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                    Console.ReadKey();
+                }
+            }
+
+            Console.Read();
+
+
+        }
+    }
+}
+
